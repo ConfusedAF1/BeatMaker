fb1329d1039ec63903695302774b353090af5fb9
ConfusedAF1 <149064103+ConfusedAF1@users.noreply.github.com>
2023-10-26 17:23:30 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,171 @@
+class DrumKit {
+    constructor() {
+      this.pads = document.querySelectorAll(".pad");
+      this.playBtn = document.querySelector(".play");
+      this.currentKick = "./sounds/kick-classic.wav";
+      this.currentSnare = "./sounds/snare-acoustic01.wav";
+      this.currentHihat = "./sounds/hihat-acoustic01.wav";
+      this.kickAudio = document.querySelector(".kick-sound");
+      this.snareAudio = document.querySelector(".snare-sound");
+      this.hihatAudio = document.querySelector(".hihat-sound");
+      this.index = 0;
+      this.bpm = 150;
+      this.isPlaying = null;
+      this.selects = document.querySelectorAll("select");
+      this.muteBtns = document.querySelectorAll(".mute");
+      this.tempoSlider = document.querySelector(".tempo-slider");
+    }
+    activePad() {
+      this.classList.toggle("active");
+    }
+    repeat() {
+      let step = this.index % 8;
+      const activeBars = document.querySelectorAll(`.b${step}`);
+      //Loop over the pads
+      activeBars.forEach(bar => {
+        bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+        //Check if pads are active
+        if (bar.classList.contains("active")) {
+          if (bar.classList.contains("kick-pad")) {
+            this.kickAudio.currentTime = 0;
+            this.kickAudio.play();
+          }
+          if (bar.classList.contains("snare-pad")) {
+            this.snareAudio.currentTime = 0;
+            this.snareAudio.play();
+          }
+          if (bar.classList.contains("hihat-pad")) {
+            this.hihatAudio.currentTime = 0;
+            this.hihatAudio.play();
+          }
+        }
+      });
+      this.index++;
+    }
+    start() {
+      const interval = (60 / this.bpm) * 1000;
+      //Check if it's playing
+  
+      if (this.isPlaying) {
+        //Clear the interval
+        clearInterval(this.isPlaying);
+        console.log(this.isPlaying);
+        this.isPlaying = null;
+      } else {
+        this.isPlaying = setInterval(() => {
+          //Envoking our repeat function
+          this.repeat();
+        }, interval);
+      }
+    }
+    updateBtn() {
+      //NULL
+  
+      if (!this.isPlaying) {
+        this.playBtn.innerText = "Stop";
+        this.playBtn.classList.add("active");
+      } else {
+        this.playBtn.innerText = "Play";
+        this.playBtn.classList.remove("active");
+      }
+    }
+    changeSound(e) {
+      const selectionName = e.target.name;
+      const selectionValue = e.target.value;
+      switch(selectionName) {
+        case "kick-select" :
+          this.kickAudio.src = selectionValue;
+          break; 
+        case "snare-select" :
+          this.snareAudio.src = selectionValue;
+          break; 
+        case "hihat-select" :
+          this.hihatAudio.src = selectionValue;
+          break; 
+      }
+    }
+    mute(e){
+      const muteIndex = e.target.getAttribute("data-track");
+      e.target.classList.toggle("active");
+      if (e.target.classList.contains("active")){
+        switch(muteIndex) {
+          case "0":
+            this.kickAudio.volume = 0;
+            break;
+          case "1":
+            this.snareAudio.volume = 0;
+            break;
+          case "2":
+            this.hihatAudio.volume = 0;
+            break;
+        }
+      } else {
+        switch(muteIndex) {
+          case "0":
+            this.kickAudio.volume = 1;
+            break;
+          case "1":
+            this.snareAudio.volume = 1;
+            break;
+          case "2":
+            this.hihatAudio.volume = 1;
+            break;
+        }
+      }
+    }
+    changeTempo(e){
+      const tempoText = document.querySelector(".tempo-nr");
+      tempoText.innerText = e.target.value;
+    }
+    updateTempo(e){
+      //Updating our bpm 
+      this.bpm = e.target.value;
+      //clearing interval as it has older bpm set to it
+      clearInterval(this.isPlaying);
+      this.isPlaying = null;
+      const playBtn = document.querySelector(".play");
+      if (playBtn.classList.contains("active")) {
+        //Starting to play our sound with newly updated bpm
+        this.start();
+      }
+    }
+}
+  
+const drumKit = new DrumKit();
+  
+//No need for arrow function or function keyword as we are just adding class in activePad function
+
+// ----**Event Listner**----
+drumKit.pads.forEach(pad => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function() {
+    this.style.animation = "";
+    });
+  });
+
+//function keyword is added to make this keyword refer to DrumKit, not to playBtn
+// Takeaway : When adding Event Listeners add function keyword/arrow function[()=>] and then envoke you method.
+// Takeaway : When having problem what does this keyword refers to just console log it
+  
+drumKit.playBtn.addEventListener("click", function() {
+  drumKit.updateBtn();
+  drumKit.start();
+  });
+
+drumKit.selects.forEach(select => {
+// Used function keyword as wanted to add event(e)
+  select.addEventListener('change', function(e){
+    drumKit.changeSound(e);
+  });
+});
+drumKit.muteBtns.forEach(btn => {
+  btn.addEventListener("click", function(e){
+    drumKit.mute(e);
+  });
+});
+drumKit.tempoSlider.addEventListener("input", function(e){
+  drumKit.changeTempo(e);
+});
+drumKit.tempoSlider.addEventListener("change", function(e){
+  drumKit.updateTempo(e);
+});
